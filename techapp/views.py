6694ea42b89e3bcb86008acb3115a128f8033185
ward.zhang@quectel.com <ward.zhang@quectel.com>
2024-01-30 11:27:38 +0800

    表单提交

--- techapp/views.py
+++ techapp/views.py
@@ -6,3 +6,9 @@ from django.shortcuts import render
 def index(request):
     context = {}
     return render(request, "techapp/index.html", context)
+
+
+def message(request):
+    print(request.POST["message"], request.POST["name"], request.POST["email"])
+    context = {}
+    return render(request, "techapp/index.html", context)
